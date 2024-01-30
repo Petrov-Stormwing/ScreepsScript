1d1b821f34f706d43294c68b75d621354a57011f
Petrov-Stormwing <selina.tsukiomi@gmail.com>
2024-01-30 10:39:06 +0200

    Implemented functions to get nearest container and extension, as well as function getStorage

--- Utils.js
+++ Utils.js
@@ -1,7 +1,9 @@
 global.getSpawner = getSpawner;
 global.getContainers = getContainers;
 global.getExtensions = getExtensions;
-
+global.getNearestContainer=getNearestContainer;
+global.getNearestExtension=getNearestExtension;
+global.getStorage=getStorage;
 
 function getSpawner(creep) {
     return creep.room.find(FIND_MY_SPAWNS)[0];
@@ -18,4 +20,28 @@ function getExtensions(creep) {
     return creep.room.find(FIND_STRUCTURES, {
         filter: (structure) => structure.structureType === STRUCTURE_EXTENSION
     });
+}
+
+function getStorage(creep) {
+    return creep.room.find(FIND_STRUCTURES, {
+        filter: (structure) => structure.structureType === STRUCTURE_STORAGE
+    });
+}
+
+function getNearestContainer(creep){
+    let containers=getContainers(creep)
+    if(containers.length>0){
+        return creep.pos.findClosestByPath(containers);
+    }else {
+        console.log("No Containers in this Room")
+    }
+}
+
+function getNearestExtension(creep){
+    let extensions=getExtensions(creep)
+    if(extensions.length>0){
+        return creep.pos.findClosestByPath(extensions);
+    }else {
+        console.log(`No Extensions in Room: ${creep.room.name}`)
+    }
 }
